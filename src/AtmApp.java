dd0f58a7c6ad3e259ee0135468af1af12c41d235
Mehmet Karagöz <karagoz12170@gmail.com>
2023-04-26 13:17:47 +0300

    mht1

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -2,5 +2,7 @@ public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir atm projesidir");
         //version1
+        System.out.println("mht kodlarını yazdı.");
+        //mht1
     }
 }
